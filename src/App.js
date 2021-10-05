29f2450bed00c30bdeadc3658f319f0f8656c023
SaikrupaSiragam <86454447+SaikrupaSiragam@users.noreply.github.com>
2021-10-05 21:30:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculator</h1>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
